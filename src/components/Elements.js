50af78da0b1ea6199f65f5baffe0ec804ef9c125
baodainguyen <baodaihd89@gmail.com>
2021-11-12 21:44:14 +0700

    update code

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -17,13 +17,18 @@ export class Footer extends Component {
 class ListTechnology extends Component {
     render() {
         return (
-            <div style={{marginTop: '69px'}}>
-                <div className="d-flex">
+            <div style={{ marginTop: '69px' }}>
+                <Row >
                     {Logos.map((logoItem, i) => (
                         <TechnologyItem logo={logoItem} key={i} />
                     ))}
                     <TechnologyGroupLogo />
-                </div>
+                    <Col lg={2} md={4} sm={6}>
+                        <h3>Get Started Quicker</h3>
+                        <p>Get inspiration from Pens using frameworks, libraries, and design patterns. Then, start your own with premade templates.</p>
+                        <Button variant="dark">Explore Topics</Button>{' '}
+                    </Col>
+                </Row>
             </div>
         );
     }
@@ -32,10 +37,12 @@ class TechnologyItem extends Component {
     render() {
         const { src, name } = this.props.logo;
         return (
-            <div className="dnb-tech-item rounded">
-                <div><img src={src} alt={`${name} Logo`} /></div>
-                <h3>{name}</h3>
-            </div>
+            <Col lg={2} md={4} sm={6}>
+                <div className="dnb-tech-item rounded">
+                    <div><img src={src} alt={`${name} Logo`} /></div>
+                    <h3>{name}</h3>
+                </div>
+            </Col>
         );
     }
 }
@@ -43,14 +50,16 @@ class TechnologyGroupLogo extends Component {
     render() {
         const name = `+ ${GroupLogos.length}`;
         return (
-            <div className="dnb-tech-item rounded">
-                <div className="d-flex flex-column" style={{maxHeight: '131px'}}>
-                    {GroupLogos.map((logoItem, i) => (
-                        <img src={logoItem.src} alt={`${logoItem.name} Logo`} key={i}/>
-                    ))}
+            <Col lg={2} md={4} sm={6}>
+                <div className="dnb-tech-item rounded">
+                    <div className="d-flex flex-column dnb-tech-item-group" style={{ maxHeight: '131px' }}>
+                        {GroupLogos.map((logoItem, i) => (
+                            <img src={logoItem.src} alt={`${logoItem.name} Logo`} key={i} />
+                        ))}
+                    </div>
+                    <h3>{name}</h3>
                 </div>
-                <h3>{name}</h3>
-            </div>
+            </Col>
         );
     }
 }
@@ -61,7 +70,7 @@ export class PreviewGrid extends Component {
         return (
             <Container>
                 <Row>
-                    <Col md={4} xs={12} className="d-flex align-items-center">
+                    <Col lg={4} md={12} className="d-flex align-items-center mb-5">
                         <div>
                             <h5>- Super Easy way to build perfect website</h5>
                             <h3>Meet Wizixo - Creative Agency Multi-Purpose Theme</h3>
@@ -69,17 +78,17 @@ export class PreviewGrid extends Component {
                             <Button variant="dark">Purchase Now!</Button>{' '}
                         </div>
                     </Col>
-                    <Col md={8} xs={12}>
+                    <Col lg={8} md={12}>
                         <div className="position-relative w-100 dnb-card-group" style={{ backgroundColor: '#ffffff' }}>
-                            <div className="position-absolute rounded dnb-card1" style={{ backgroundColor: 'cyan' }}></div>
-                            <div className="position-absolute rounded dnb-card2" style={{ backgroundColor: 'pink' }}></div>
-                            <div className="position-absolute rounded dnb-card3" style={{ backgroundColor: 'blue' }}></div>
-                            <div className="position-absolute rounded dnb-card4" style={{ backgroundColor: 'green' }}></div>
-                            <div className="position-absolute rounded dnb-card5" style={{ backgroundColor: 'yellow' }}></div>
-                            <div className="position-absolute rounded dnb-card6" style={{ backgroundColor: 'cyan' }}></div>
-                            <div className="position-absolute rounded dnb-card7" style={{ backgroundColor: 'pink' }}></div>
-                            <div className="position-absolute rounded dnb-card8" style={{ backgroundColor: 'green' }}></div>
-                            <div className="position-absolute rounded dnb-card9" style={{ backgroundColor: 'blue' }}></div>
+                            <div className="dnb-card rounded dnb-card1" style={{ backgroundColor: 'cyan' }}></div>
+                            <div className="dnb-card rounded dnb-card2" style={{ backgroundColor: 'pink' }}></div>
+                            <div className="dnb-card rounded dnb-card3" style={{ backgroundColor: 'blue' }}></div>
+                            <div className="dnb-card rounded dnb-card4" style={{ backgroundColor: 'green' }}></div>
+                            <div className="dnb-card rounded dnb-card5" style={{ backgroundColor: 'yellow' }}></div>
+                            <div className="dnb-card rounded dnb-card6" style={{ backgroundColor: 'cyan' }}></div>
+                            <div className="dnb-card rounded dnb-card7" style={{ backgroundColor: 'pink' }}></div>
+                            <div className="dnb-card rounded dnb-card8" style={{ backgroundColor: 'green' }}></div>
+                            <div className="dnb-card rounded dnb-card9" style={{ backgroundColor: 'blue' }}></div>
                         </div>
                     </Col>
                 </Row>
