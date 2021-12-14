a53201039601de6ba0a6181858b2c278fec02849
baodainguyen <baodaihd89@gmail.com>
2021-12-14 22:49:05 +0700

    update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -104,9 +104,7 @@ class ItemGlide extends Component {
 }
 
 class Section03 extends Component {
-    state = Object.assign({
-        width: window.innerWidth
-    }, DataSection03);
+    state = Object.assign({}, DataSection03);
 
     componentDidMount() {
         DataSection03.getText().then(data => {
@@ -118,28 +116,18 @@ class Section03 extends Component {
         DataSection03.getAllCards().then(cards => {
             this.setState({ cards: cards });
         });
-        window.addEventListener('resize', this.onResize);
-    }
-    onResize = () => {
-        this.setState({ width: window.innerWidth });
-    }
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onResize);
     }
 
     render() {
-        const { title, head, cards, width } = this.state;
+        const { title, head, cards } = this.state;
         const _head = head ? <h4 className="mt-3">{head}</h4> : <></>;
         var _cards01 = cards.slice(0, 2);
         var _cards02 = cards.slice(2);
-        if (head && 767 < width && width < 992) {   // md={6}
-            _cards01 = cards.slice(0, 4);
-            _cards02 = cards.slice(4);
-        }
+
         return (<>
             <ColSection03 cards={_cards01} title={title} />
-            <Container className="my-5">
-                <Row className="py-3">
+            <Container className="my-2">
+                <Row className="py-1">
                     {_head}
                     <ColSection04 cards={_cards02} />
                 </Row>
@@ -149,69 +137,103 @@ class Section03 extends Component {
     }
 }
 class ColSection03 extends Component {
+    state = { width: window.innerWidth }
+
+    onResize = () => {
+        this.setState({ width: window.innerWidth });
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize);
+    }
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize);
+    }
+
     render() {
         const { title, cards } = this.props;
+        const { width } = this.state;
+        var height = 510, sub = 51;
+        if (width < 992) {   // md={6}
+            sub = -450
+        }
         return (
             <Container fluid className='px-0 py-5'>
                 <Container><h3 className="fw-bold">{title}</h3></Container>
                 <Row className="gx-0">
                     {cards.map((card, i) => {
-                        return <ColSection03Side isLeft={i % 2 == 0} card={card} />
+                        return <ColSection03SideBg isLeft={i % 2 == 0} card={card} height={`${height}px`} />
                     })}
                 </Row>
+                <Container style={{ marginTop: `-${height - sub}px`, minHeight: `${height - sub}px`, maxHeight: `${height - sub}px` }}>
+                    <Row>
+                        {cards.map((card, i) => {
+                            return <ColSection03Side isLeft={i % 2 == 0} card={card} height={`${height - 100}px`} />
+                        })}
+                    </Row>
+                </Container>
             </Container>
         );
     }
 }
-class ColSection03Side extends Component {
-    state = {
-        bg: 'rgb(0,0,0)', color: 'rgb(255,255,255)'
-    }
+class ColSection03SideBg extends Component {
+    state = { bg: 'rgb(0,0,0)' }
     onLoad = (e) => {
         const rgb = getAverageRGB(e);
-        const bg = `rgb(${rgb.r},${rgb.g},${rgb.b})`;
-        const color = getLuminanceFrom(rgb.r, rgb.g, rgb.b);
         this.setState({
-            bg: bg,
-            color: color
+            bg: `rgb(${rgb.r},${rgb.g},${rgb.b})`
         });
     }
     render() {
-        const { card, isLeft } = this.props;
-        const {bg, color} = this.state;
+        const { card, isLeft, height } = this.props;
+        const { bg } = this.state;
+        const view = isLeft ? <img src={card.img} onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous" /> :
+            <img src={card.img} onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous" />;
+
+        return (
+            <Col md={12} lg={6} >
+                <Card className='border-0 rounded-0'
+                    style={{ backgroundColor: bg, minHeight: height, maxHeight: height }}>
+                    <div style={{ opacity: '0' }}>{view}</div>
+                </Card>
+            </Col>
+        );
+    }
+}
+class ColSection03Side extends Component {
+    state = { color: 'rgb(255,255,255)' }
+    onLoad = (e) => {
+        const rgb = getAverageRGB(e);
+        this.setState({
+            color: getLuminanceFrom(rgb.r, rgb.g, rgb.b)
+        });
+    }
+    render() {
+        const { card, isLeft, height } = this.props;
+        const { color } = this.state;
         const view = isLeft ? <>
-            <Col md={3}></Col>
-            <Col md={9} className='ms-n4'>
-                <Row className='gx-0'>
-                    <Col md={4}>
-                        <h5>{card.title}</h5>
-                        <p>{card.des}</p>
-                    </Col>
-                    <Col md={8} className='ms-4 me-n4'>
-                        <img className='w-100' src={card.img} onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous"/>
-                    </Col>
-                </Row>
+            <Col xs={3}>
+                <h5>{card.title}</h5>
+                <p>{card.des}</p>
+            </Col>
+            <Col xs={9} className='rounded-2 overflow-hidden mb-5 pb-5'>
+                <img style={{ maxHeight: height }} className='rounded-2' src={card.img}
+                    onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous" />
             </Col>
         </> : <>
-            <Col md={9}>
-                <Row className='gx-0'>
-                    <Col md={8}>
-                        <img className='w-100' src="https://images.squarespace-cdn.com/content/v1/54fc8146e4b02a22841f4df7/1627654577989-RXF9XFY4M6BKXNUP9YB6/Art_of_Iris_Compiet_1+%2811%29.jpg"
-                        onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous"/>
-                    </Col>
-                    <Col md={4}>
-                        <h5>{card.title}</h5>
-                        <p>{card.des}</p>
-                    </Col>
-                </Row>
+            <Col xs={9} className='rounded-2 overflow-hidden text-end'>
+                <img style={{ maxHeight: height }} className='rounded-2' src={card.img}
+                    onLoad={e => { this.onLoad(e.target) }} crossOrigin="anonymous" />
             </Col>
-            <Col md={3}></Col>
+            <Col xs={3} className='text-end'>
+                <h5>{card.title}</h5>
+                <p>{card.des}</p>
+            </Col>
         </>;
 
         return (
-            <Col xs={12} md={6} >
-                <Card className='border-0 rounded-0' style={{backgroundColor: bg}}>
-                    <Row className='g-0' style={{color: color}}>
+            <Col md={12} lg={6} >
+                <Card className='border-0 rounded-0 h-100' style={{ backgroundColor: 'transparent' }}>
+                    <Row className='g-2' style={{ color: color }}>
                         {view}
                     </Row>
                 </Card>
